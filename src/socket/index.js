fb502c0a02fafc30bb89e0d69dadb9a8ef5b42e8
Костя Сарвилов <kostyasarvilov@MacBook-Pro-Kosta.local>
2023-07-24 01:38:27 +0300

    deploy

--- src/socket/index.js
+++ src/socket/index.js
@@ -4,10 +4,9 @@ const options = {
   "force new connection": true,
   reconnectionAttempts: "Infinity",
   timeout: 10000,
-  secure: true,
   transports: ["websocket"],
 };
 
-const socket = io("/", options);
+const socket = io("http://localhost:3001", options);
 
 export default socket;
